245005e1d2cbb27326aaefbde0e2690cabbe20db
Delthi <32238382+Delthi@users.noreply.github.com>
2021-12-01 08:25:08 +0100

    Add project files.

---
+++ AoC2021/Day01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2021
+{
+    public class Day01
+    {
+        public void PartA(string[] input)
+        {
+            var numbers = input.Select(x => int.Parse(x)).ToArray();
+            int cnt = 0;
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > numbers[i - 1]) cnt++;
+            }
+
+            Console.WriteLine(cnt);
+        }
+
+        public void PartB(string[] input)
+        {
+            var numbers = input.Select(x => int.Parse(x)).ToArray();
+            int cnt = 0;
+            for (int i = 3; i < numbers.Length; i++)
+            {
+                int sum1 = numbers[i - 3] + numbers[i - 2] + numbers[i - 1];
+                int sum2 = numbers[i - 2] + numbers[i - 1] + numbers[i];
+                if (sum2 > sum1) cnt++;
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ AoC2021/Program.cs
@@ -0,0 +1,10 @@
+﻿using AoC2021;
+
+var day01 = new Day01();
+day01.PartA(GetInput(1));
+
+string[] GetInput(int day)
+{
+    string daystr = day < 10 ? "0" + day : day.ToString();
+    return File.ReadAllLines($"..\\..\\..\\input\\in{daystr}.txt");
+}
