6b8cce9dd02dd3f743549a26aa956c37b1293eba
Delthi <32238382+Delthi@users.noreply.github.com>
2021-12-06 10:29:30 +0100

    Day 6

--- AoC2021/Day06.cs
+++ AoC2021/Day06.cs
@@ -10,13 +10,58 @@ namespace AoC2021
     {
         public void PartA(string[] lines)
         {
+            var fishes = lines[0].Split(',').Select(l => int.Parse(l)).ToList();
+            for(int i = 0; i < 80; ++i)
+            {
+                for(int j = 0; j < fishes.Count; ++j)
+                {
+                    if(fishes[j] == 0)
+                    {
+                        fishes[j] = 6;
+                        fishes.Add(9);
+                    }
+                    else
+                    {
+                        --fishes[j];
+                    }
+                }
+            }
 
+            Console.WriteLine(fishes.Count);
         }
 
         public void PartB(string[] lines)
         {
+            var fishes = lines[0].Split(',').Select(l => long.Parse(l)).ToList();
 
+            var dict = new Dictionary<long, long>();
+            for(int i = 0; i < 9; ++i)
+            {
+                dict[i] = 0;
+            }
+
+            foreach(var fish in fishes)
+            {
+                dict[fish]++;
+            }
+
+            for(int i = 0; i < 256; ++i)
+            {
+                var oldState = dict.ToDictionary(x => x.Key, x => x.Value);
+                dict[8] = oldState[0];
+                dict[7] = oldState[8];
+                dict[6] = oldState[7] + oldState[0];
+                dict[5] = oldState[6];
+                dict[4] = oldState[5];
+                dict[3] = oldState[4];
+                dict[2] = oldState[3];
+                dict[1] = oldState[2];
+                dict[0] = oldState[1];
+            }
+
+            Console.WriteLine(dict.Values.Sum()); 
         }
 
+        
     }
 }
--- AoC2021/Program.cs
+++ AoC2021/Program.cs
@@ -1,32 +1,10 @@
 ﻿using AoC2021;
-/*
-var day01 = new Day01();
-day01.PartA(GetInput(1));
 
-var day02 = new Day02();
-day02.PartA(GetInput(2));
-day02.PartB(GetInput(2));
-
-var day03 = new Day03();
-day03.PartA(GetInput(3));
-day03.PartB(GetInput(3));
-
-var day04 = new Day04();
-day04.PartA(GetInput(4));
-day04.PartB(GetInput(4));
-
-var day05 = new Day05();
-day05.PartA(GetInput(5));
-day05.PartB(GetInput(5));
-*/
-
-var day06 = new Day06();
-Console.Write("Test ");
-day06.PartA(GetTestInput(6));
-day06.PartA(GetInput(6));
-Console.Write("Test ");
-day06.PartB(GetTestInput(6));
-day06.PartB(GetInput(6));
+var day07 = new Day06();
+day07.PartA(GetTestInput(7));
+//day06.PartA(GetInput(7));
+day07.PartB(GetTestInput(7));
+//day06.PartB(GetInput(7));
 
 string[] GetInput(int day)
 {
@@ -37,5 +15,5 @@ string[] GetInput(int day)
 string[] GetTestInput(int day)
 {
     string daystr = day < 10 ? "0" + day : day.ToString();
-    return File.ReadAllLines($"..\\..\\..\\input\\in{daystr}test.txt");
+    return File.ReadAllLines($"..\\..\\..\\testinput\\in{daystr}test.txt");
 }
