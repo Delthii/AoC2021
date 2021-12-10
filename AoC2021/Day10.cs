b2778b84bc06e018b8a0791f6e2c4c3d0a928663
Delthi <32238382+Delthi@users.noreply.github.com>
2021-12-10 08:24:37 +0100

    Day10

--- AoC2021/Day10.cs
+++ AoC2021/Day10.cs
@@ -10,10 +10,130 @@ namespace AoC2021
     {
         public void PartA(string[] lines)
         {
+            Dictionary<char, char> lookup = new Dictionary<char, char>();
+            lookup[')'] = '(';
+            lookup[']'] = '[';
+            lookup['}'] = '{';
+            lookup['>'] = '<';
+            List<char> incorrect = new List<char>();
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+                var correct = true;
+                foreach(var c in line)
+                {
+                    if (!IsClosing(c, lookup))
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        correct = lookup[c] == stack.Peek();
+                        if (correct)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            incorrect.Add(c);
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            Console.WriteLine(incorrect.Sum(x => ToInt(x)));
         }
 
         public void PartB(string[] lines)
         {
+            Dictionary<char, char> lookup = new Dictionary<char, char>();
+            lookup[')'] = '(';
+            lookup[']'] = '[';
+            lookup['}'] = '{';
+            lookup['>'] = '<';
+            List<char> incorrect = new List<char>();
+            var ans = new List<long>();
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+                var correct = true;
+                foreach (var c in line)
+                {
+                    if (!IsClosing(c, lookup))
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        correct = lookup[c] == stack.Peek();
+                        if (correct)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            incorrect.Add(c);
+                            break;
+                        }
+                    }
+                }
+                if (correct)
+                {
+                    var reverseLookup = new Dictionary<char, char>();
+                    var sum = 0L;
+                    reverseLookup['('] = ')';
+                    reverseLookup['['] = ']';
+                    reverseLookup['{'] = '}';
+                    reverseLookup['<'] = '>';
+                    while (stack.Count > 0)
+                    {
+                        sum = sum * 5 + ToIntB(stack.Pop());
+                    }
+                    ans.Add(sum);
+                }
+            }
+            ans.Sort();
+            Console.WriteLine(ans[ans.Count / 2]);
+        }
+
+        private int ToInt(char c)
+        {
+            switch (c)
+            {
+                case ')':
+                    return 3;
+                case ']':
+                    return 57;
+                case '}':
+                    return 1197;
+                case '>':
+                    return 25137;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private int ToIntB(char c)
+        {
+            switch (c)
+            {
+                case '(':
+                    return 1;
+                case '[':
+                    return 2;
+                case '{':
+                    return 3;
+                case '<':
+                    return 4;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private bool IsClosing(char c, Dictionary<char, char> lookup)
+        {
+            return lookup.Keys.Contains(c);
         }
     }
 }
