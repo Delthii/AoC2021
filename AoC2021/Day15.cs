0daef3a19ed020ee84555a81bf26f890c41d1014
Delthi <32238382+Delthi@users.noreply.github.com>
2021-12-15 11:54:36 +0100

    Day 15 with Astar

--- AoC2021/Day15.cs
+++ AoC2021/Day15.cs
@@ -12,101 +12,39 @@ namespace AoC2021
         public void PartA(string[] lines)
         {
             var grid = new Grid<int>(lines.Select(line => line.Select(c => c - '0').ToArray()).ToArray());
-            var dist = new Dictionary<(int x, int y), int>();
-            var prev = new Dictionary<(int x, int y), (int x, int y)?>();
-            var prioQ = new PriorityQueue<(int x, int y), int>();
-            dist[(0, 0)] = 0;
-            for (int x = 0; x < grid.Width; x++)
-            {
-                for (int y = 0; y < grid.Height; y++)
-                {
-                    var t = (x, y);
-                    if (x != 0 || y != 0)
-                    {
-                        dist[t] = int.MaxValue;
-                        prev[t] = null;
-                    }
+            var (path, cost) = grid.GetClosestPath((0, 0), (grid.Width - 1, grid.Height - 1), Distance, Hueristic);
 
-                    prioQ.Enqueue(t, dist[t]);
-                }
-            }
-
-            while(prioQ.Count > 0)
-            {
-                var u = prioQ.Dequeue();
-                foreach (var n in grid.GetN4(u.x, u.y))
-                {
-                    var (x, y) = (n.X, n.Y);
-                    var alt = dist[u] + grid.Get(x, y);
-                    if(alt < dist[(x, y)])
-                    {
-                        dist[(x, y)] = alt;
-                        prev[(x, y)] = u;
-                        prioQ.Enqueue((x, y), alt);
-                    }
-                }
-            }
-
-            Console.WriteLine(dist[(grid.Width - 1, grid.Height - 1)]);
+            Console.WriteLine(cost);
         }
 
         public void PartB(string[] lines)
         {
             var map = lines.Select(line => line.Select(c => c - '0').ToArray()).ToArray();
-            var temp = map.Select(row => Duplicate(row)).ToArray();
+            var temp = map.Select(row => X5(row)).ToArray();
             List<int[][]> tempList = new();
+
             for(int i = 0; i < 5; i++)
             {
                 tempList.Add(temp.Select(row => row.Select(c => Inc(c, i)).ToArray()).ToArray());
             }
-            var temp2 = tempList.SelectMany(list => list).ToArray();
+
             var grid = new Grid<int>(tempList.SelectMany(list => list).ToArray());
-            var dist = new Dictionary<(int x, int y), int>();
-            var prev = new Dictionary<(int x, int y), (int x, int y)?>();
-            var prioQ = new PriorityQueue<(int x, int y), int>();
-            dist[(0, 0)] = 0;
-            for (int x = 0; x < grid.Width; x++)
-            {
-                for (int y = 0; y < grid.Height; y++)
-                {
-                    var t = (x, y);
-                    if (x != 0 || y != 0)
-                    {
-                        dist[t] = int.MaxValue;
-                        prev[t] = null;
-                    }
-
-                    prioQ.Enqueue(t, dist[t]);
-                }
-            }
-
-            while (prioQ.Count > 0)
-            {
-                var u = prioQ.Dequeue();
-                foreach (var n in grid.GetN4(u.x, u.y))
-                {
-                    var (x, y) = (n.X, n.Y);
-                    var alt = dist[u] + grid.Get(x, y);
-                    if (alt < dist[(x, y)])
-                    {
-                        dist[(x, y)] = alt;
-                        prev[(x, y)] = u;
-                        prioQ.Enqueue((x, y), alt);
-                    }
-                }
-            }
-
-            Console.WriteLine(dist[(grid.Width - 1, grid.Height - 1)]);
+            var (path, cost) = grid.GetClosestPath((0, 0), (grid.Width - 1, grid.Height - 1), Distance, Hueristic);
+            
+            Console.WriteLine(cost);
         }
 
-        private int Inc(int c, int i)
+        private int Distance((int X, int Y) start, (int X, int Y) end, IGrid<int> grid)
         {
-            var temp = c + i;
-            if(temp > 9) temp -= 9;
-            return temp;
+            return grid.Get(end.X, end.Y);
         }
 
-        private int[] Duplicate(int[] row)
+        private int Hueristic((int X, int Y) node, IGrid<int> grid)
+        {
+            return grid.Width - node.X + grid.Height - node.Y;
+        }
+
+        private int[] X5(int[] row)
         {
             var arr = new int[row.Length * 5];
             for(int i = 0; i < 5; ++i)
@@ -114,12 +52,12 @@ namespace AoC2021
                 for(int j = 0; j < row.Length; ++j)
                 {
                     var index = i * row.Length + j;
-                    arr[index] = (row[j] + i);
-                    if (arr[index] > 9) arr[index] -= 9;
+                    arr[index] = Inc(row[j], i);
                 }
             }
 
             return arr;
         }
+        private int Inc(int c, int i) => c + i > 9 ? c + i - 9 : c + i;
     }
 }
--- Utils/IGrid.cs
+++ Utils/IGrid.cs
@@ -2,9 +2,12 @@
 {
     public interface IGrid<T>
     {
+        public int Width { get; }
+        public int Height { get; }
         void Set(int x, int y, T item);
         bool TrySet(int x, int y, T item);
         IEnumerable<IGridNode<T>> GetConnectedComponent(int x, int y, Func<T, bool> predicate);
+        (IEnumerable<IGridNode<T>>, int Cost) GetClosestPath(int sx, int sy, int ex, int ey, Func<(int X, int Y), (int X, int Y), IGrid<T>, int> cost);
         T Get(int x, int y);
         IGridNode<T> GetNode(int x, int y);
         bool TryGet(int x, int y, out T? item);
